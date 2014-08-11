7a357e480ae5ed052afbdca423f42414e94fe401
Begreattoplay <letmepicksomething@gmail.com>
2014-08-11 11:38:37 -0500

    Initial check in

---
+++ AngularJSWithWebAPI/app/scripts/app.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+
+angular.module('rcForm', []).directive(rcSubmitDirective);
+
+var app = angular.module('webApp', [
+    'ngCookies',
+    'ngSanitize',
+    'ngRoute',
+    'ui.bootstrap',
+    'rcForm',
+    'rcDisabledBootstrap'])
+
+app.config(function ($routeProvider, $httpProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/app/views/Home.html',
+        controller: 'HomeController',
+        title: 'Home',
+        allowAnon: true
+    })
+    .when('/requireauth', {
+        templateUrl: '/app/views/RequireAuth.html',
+        controller: 'AuthController',
+        title: 'Auth Page'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+
+    $httpProvider.interceptors.push('AuthInterceptor');
+});
+
+app.run(function ($rootScope, SessionHandler, $location, $route) {
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        if ((next && next.$$route.allowAnon) ||
+            SessionHandler.checkLoggedIn() && ($location.path() !== '' && $location.path() !== '/')) {
+            $location.path();
+        }
+    })
+
+    $rootScope.$on("$routeChangeSuccess", function (currentRoute, previousRoute) {
+        $rootScope.title = $route.current.title;
+    });
+});
