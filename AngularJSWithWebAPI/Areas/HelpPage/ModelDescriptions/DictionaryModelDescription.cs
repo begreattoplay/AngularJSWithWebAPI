4404399a2d0042f01ddca4b5ca06df1d9384eb94
Begreattoplay <letmepicksomething@gmail.com>
2014-08-11 11:43:24 -0500

    Revert "Initial check in"
    This reverts commit 7a357e480ae5ed052afbdca423f42414e94fe401.

--- AngularJSWithWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AngularJSWithWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- AngularJSWithWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AngularJSWithWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
